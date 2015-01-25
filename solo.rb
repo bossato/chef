c419c49584784d6da7b964824a87d9f66ab76446
Kazuya Sato <recoba0110@gmail.com>
2015-01-25 12:18:29 +0900

    Add solo.rb #5

---
+++ solo.rb
@@ -0,0 +1,5 @@
+file_cache_path           "/tmp/chef-repo"
+data_bag_path             "/root/chef-repo/data_bags"
+encrypted_data_bag_secret "/root/chef-repo/data_bag_key"
+cookbook_path             [ "/root/chef-repo/site-cookbooks", "/root/chef-repo/cookbooks" ]
+role_path                 "/root/chef-repo/roles"
